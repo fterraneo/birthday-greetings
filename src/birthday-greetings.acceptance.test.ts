f5e38bdebbbcf255ca224a42759f95a57370c2e0
Federico Terraneo <f.terraneo@hotmail.com>
2025-03-18 10:02:27 +0100

    can prepare and read employees csv files

--- src/birthday-greetings.acceptance.test.ts
+++ src/birthday-greetings.acceptance.test.ts
@@ -1,6 +1,9 @@
 import { test, expect, beforeEach, afterEach } from "@jest/globals"
 import { LocalSmtpServer } from "./support/local-smtp-server"
 import { sendMail } from "./birthday-greetings"
+import { EOL } from "os"
+import { writeFileSync } from "fs"
+import { readFile } from "fs/promises"
 
 const smtpConfig = {
     hostname: "0.0.0.0",
@@ -24,4 +27,28 @@ test("send mail", async () => {
     const messages = await localSmtpServer.deliveredMessages()
     expect(messages?.count).toBe(1)
 })
+
+test("read employees csv file", async () => {
+    const data = ["Herman, Toothrot, 1960-07-01, h.t.marley@monkey.com"]
+    const fileName = 'testfiles/employees.test.csv'
+    prepareEmployeesCsv(fileName, data)
+
+    const content = await readEmployeesCsv(fileName)
+
+    expect(content).toEqual(data)
+})
+
+function prepareEmployeesCsv(fileName: string, data: string[]) {
+    const header = "last_name, first_name, date_of_birth, email";
+    const allData = [header].concat(data).join(EOL);
+    writeFileSync(fileName, allData)
+}
+
+async function readEmployeesCsv(fileName: string) {
+    const buffer = await readFile(fileName)
+    const content = buffer.toString()
+    const allLines = content.split(EOL)
+    const [, ...employeeLines] = allLines
+    return employeeLines;
+}
 
