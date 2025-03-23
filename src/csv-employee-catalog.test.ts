d93ce453c3f2fd0d19389655628b2b4b25cb4d02
Federico Terraneo <f.terraneo@hotmail.com>
2025-03-23 13:04:43 +0100

    CsvEmployeeCatalog, test: many matches

--- src/csv-employee-catalog.test.ts
+++ src/csv-employee-catalog.test.ts
@@ -18,3 +18,21 @@ test("one match", async () => {
 
     expect(employees).toContainEqual(employeeFrom("Herman", "Toothrot", "1960-07-01", "h.t.marley@monkey.com"))
 })
+
+test("many matches", async () => {
+    const data = [
+        "Toothrot, Herman, 1960-07-01, h.t.marley@monkey.com",
+        "Threepwood, Guybrush, 1981-11-01, guy@monkey.com",
+        "Marley, Elaine, 1980-07-18, elaine@monkey.com"
+    ]
+    prepareEmployeesCsv(fileName, data)
+
+    const catalog = new CsvEmployeeCatalog(fileName)
+    const employees = await catalog.loadAll()
+
+    expect(employees.length).toBe(3)
+    expect(employees).toContainEqual(employeeFrom("Herman", "Toothrot", "1960-07-01", "h.t.marley@monkey.com"))
+    expect(employees).toContainEqual(employeeFrom("Guybrush", "Threepwood", "1981-11-01", "guy@monkey.com"))
+    expect(employees).toContainEqual(employeeFrom("Elaine", "Marley", "1980-07-18", "elaine@monkey.com"))
+})
+
