9bfd1b335afe3248c118cd9d9a1eb84b1b2e2b46
Federico Terraneo <f.terraneo@hotmail.com>
2025-03-23 23:58:12 +0100

    BirthdayGreetings, test, in-memory: use an in-memory test double also for PostalOffice

--- src/birthday-greetings.in-memory.test.ts
+++ src/birthday-greetings.in-memory.test.ts
@@ -1,26 +1,8 @@
-import { afterEach, beforeEach, expect, test } from "@jest/globals"
-import { BirthdayGreetings, EmployeeCatalog } from "./core/birthday-greetings"
-import { LocalSmtpServer } from "./support/local-smtp-server"
+import { expect, test } from "@jest/globals"
+import { BirthdayGreetings, EmployeeCatalog, PostalOffice } from "./core/birthday-greetings"
 import { arrayContains } from "./support/custom-asserts"
-import { mailMessageFrom } from "./core/mail-message"
-import { SmtpPostalOffice } from "./infrastructure/smtp-postal-office"
+import { MailMessage, mailMessageFrom } from "./core/mail-message"
 import { Employee, employeeFrom } from "./core/employee"
-
-const smtpConfig = {
-    hostname: "0.0.0.0",
-    smtpPort: 1028,
-    httpPort: 8028,
-}
-
-const localSmtpServer = new LocalSmtpServer(smtpConfig)
-
-beforeEach(async () => {
-    await localSmtpServer.start()
-})
-
-afterEach(() => {
-    localSmtpServer.stop()
-})
 
 class InMemoryEmployeeCatalog implements EmployeeCatalog {
     private readonly employees: Employee[]
@@ -34,18 +16,38 @@ class InMemoryEmployeeCatalog implements EmployeeCatalog {
     }
 }
 
+class InMemoryPostalOffice implements PostalOffice {
+    private readonly sentMessages: MailMessage[]
+
+    constructor() {
+        this.sentMessages = []
+    }
+
+    sendMail(mailMessage: MailMessage): Promise<void> {
+        this.sentMessages.push(mailMessage)
+        return Promise.resolve(undefined)
+    }
+
+    deliveredMessages() {
+        return {
+            count: this.sentMessages.length,
+            items: [...this.sentMessages],
+        }
+    }
+}
+
 test("no match", async () => {
     const employeeCatalog: EmployeeCatalog = new InMemoryEmployeeCatalog([
         employeeFrom("David", "Braben", "1964-01-02", "dave@frontier.com"),
         employeeFrom("Eric", "Chahi", "1967-10-21", "eric@anotherworld.com"),
         employeeFrom("Ron", "Gilbert", "1964-01-01", "ronnie@melee.com"),
     ])
-
-    const app = new BirthdayGreetings(employeeCatalog, new SmtpPostalOffice(smtpConfig))
+    const postalOffice = new InMemoryPostalOffice()
+    const app = new BirthdayGreetings(employeeCatalog, postalOffice)
 
     await app.sendGreetings(new Date("2025-03-19"))
 
-    const messages = await localSmtpServer.deliveredMessages()
+    const messages = postalOffice.deliveredMessages()
     expect(messages?.count).toBe(0)
 })
 
@@ -55,12 +57,12 @@ test("one match", async () => {
         employeeFrom("Eric", "Chahi", "1967-10-21", "eric@anotherworld.com"),
         employeeFrom("Ron", "Gilbert", "1964-01-01", "ronnie@melee.com"),
     ])
-
-    const app = new BirthdayGreetings(employeeCatalog, new SmtpPostalOffice(smtpConfig))
+    const postalOffice = new InMemoryPostalOffice()
+    const app = new BirthdayGreetings(employeeCatalog, postalOffice)
 
     await app.sendGreetings(new Date("2024-01-01"))
 
-    const messages = await localSmtpServer.deliveredMessages()
+    const messages = postalOffice.deliveredMessages()
     expect(messages?.count).toBe(1)
     expect(messages?.items).toEqual(arrayContains(mailMessageFrom("ronnie@melee.com", "Ron")))
 })
@@ -72,12 +74,12 @@ test("many matches", async () => {
         employeeFrom("Ron", "Gilbert", "1964-01-01", "ronnie@melee.com"),
         employeeFrom("Alessandro", "Sforza", "1409-10-21", "ale@sforza.it"),
     ])
-
-    const app = new BirthdayGreetings(employeeCatalog, new SmtpPostalOffice(smtpConfig))
+    const postalOffice = new InMemoryPostalOffice()
+    const app = new BirthdayGreetings(employeeCatalog, postalOffice)
 
     await app.sendGreetings(new Date("2026-10-21"))
 
-    const messages = await localSmtpServer.deliveredMessages()
+    const messages = postalOffice.deliveredMessages()
     expect(messages?.count).toBe(2)
     expect(messages?.items).toEqual(arrayContains(mailMessageFrom("eric@anotherworld.com", "Eric")))
     expect(messages?.items).toEqual(arrayContains(mailMessageFrom("ale@sforza.it", "Alessandro")))
